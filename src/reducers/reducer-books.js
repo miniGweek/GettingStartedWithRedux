cb8d48ca6c3baaf95ee1cea87beb9280cdc695fb
Rahul Sarkar <rahul.sarkar@bluestone.com.au>
2017-01-09 10:48:20 +1100

    Getting started with first Reducer and Container and glue-ing them together with redux

---
+++ src/reducers/reducer-books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [{title : 'Harry Potter', id : 1},
+            {title : 'Lord of the Rings', id : 2},
+            {title : 'Brotherhood', id : 3},
+            {title : 'Magestic', id : 4},
+            {title : 'John Carter of Mars', id : 5},
+            {title : 'Transman of Gor', id : 6}];
+}
